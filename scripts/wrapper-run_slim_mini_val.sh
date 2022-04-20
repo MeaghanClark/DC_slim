b1639c39bfd6067fd7c7597da578c99cebcacf0b
MeaghanClark <meaghaniclark@gmail.com>
2022-04-20 12:40:07 -0400

    updated mini_val running scripts to run 100 reps of K = 7500 and P = 0.2

--- scripts/wrapper-run_slim_mini_val.sh
+++ scripts/wrapper-run_slim_mini_val.sh
@@ -9,7 +9,7 @@
 storagenode=/mnt/home/clarkm89 #path to top level of dir where input/output files live
 
 # ***JOB NAME IS IN COMMAND LINE ARGUMENT*** 
-jobname=mini_Nc_test #label for SLURM book-keeping, nWF or pWF 
+jobname=btlnck_test #label for SLURM book-keeping, nWF or pWF 
 run_name=DC_slim #label to use on output files
 logfilesdir=logfiles_minival #name of directory to create and then write log files to
 executable=$storagenode/$run_name/scripts/run_slim_mini_val.sbatch #script to run 
@@ -26,10 +26,10 @@ ram_per_cpu=25G #amount of RAM to request/use per CPU
 if [ ! -d ./$logfilesdir ]; then mkdir ./$logfilesdir; fi
 
 #submit job to cluster
-for n in 4500 5500 6500 7500 8500 9500 10500 11500 ; do 
-	for p in 0.1 0.2 0.3 0.4 0.5 ; do 
+#for n in 7500 ; do  #4500 5500 6500 7500 8500 9500 10500 11500
+	for rep in {1..100} ; do  # 0.1 0.2 0.3 0.4 0.5 
 		sbatch --job-name=$jobname \
-		--export=JOBNAME=$jobname,SLIMSCRIPT=$slimscript,N=$n,P=$p,CPUS=$cpus,RUN_NAME=$run_name,STORAGENODE=$storagenode,OUTDIR=$outdir,LOGFILESDIR=$logfilesdir \
+		--export=JOBNAME=$jobname,SLIMSCRIPT=$slimscript,N=$n,P=0.2,REP=$rep,CPUS=$cpus,RUN_NAME=$run_name,STORAGENODE=$storagenode,OUTDIR=$outdir,LOGFILESDIR=$logfilesdir \
 		--cpus-per-task=$cpus \
 		--mem-per-cpu=$ram_per_cpu \
 		--output=./$logfilesdir/${jobname}_${n}_${p}_%A.out \
@@ -39,7 +39,7 @@ for n in 4500 5500 6500 7500 8500 9500 10500 11500 ; do
 		
 		echo submitting job with prob of mortality of $p and N of $n!
 	done	
-done
+#done
 
 echo ----------------------------------------------------------------------------------------
 echo My executable is $executable		
