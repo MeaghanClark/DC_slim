faecd47d0239d532fdb497d55876dc426812872a
MeaghanClark <meaghaniclark@gmail.com>
2022-05-25 11:04:16 -0400

    modifying python scripts

--- scripts/tree_2_pi.py
+++ scripts/tree_2_pi.py
@@ -7,7 +7,7 @@ import pandas as pd
 import random
 
 # uncomment these lines when running from command line
-#sys.argv = ['tree_processing.py', '../full_output/tree_nWF_full_run_42922_75.trees', '/Users/meaghan/Desktop/DC_slim/het', 'big_test']
+#sys.argv = ['tree_processing.py', '../full_output/tree_nWF_full_run_42922_75.trees', '/Users/meaghan/Desktop/DC_slim/het', 'big_test', 1e-8]
 
 seed=random.randint(1,1e6)
 print(f"random seed is {seed}")
@@ -15,12 +15,15 @@ print(f"random seed is {seed}")
 treefile = sys.argv[1]
 outdir = sys.argv[2]
 prefix = sys.argv[3]
+mu = sys.argv[4]
 
 print(f"treefile is {treefile}")
 print(f"prefix is {prefix}")
 
 # read in treefile 
 orig_ts = pyslim.load(treefile)
+
+print(f"Loaded tree file")
 
 # recapitate tree
 rts = pyslim.recapitate(orig_ts, recombination_rate = 1e-8, ancestral_Ne=4232)
@@ -31,46 +34,41 @@ print(f"Maximum number of roots before recapitation: {orig_max_roots}\n"
       f"After recapitation: {recap_max_roots}")
 
 # overlay mutations
-mts = pyslim.SlimTreeSequence(msprime.mutate(rts, rate=1.0e-8, random_seed = seed, keep=True)) 
-# should increase genome size to get more mutations
+mts = pyslim.SlimTreeSequence(msprime.mutate(rts, rate=mu, random_seed = seed, keep=True)) 
+# should increase genome size to get more mutations or set mutation rate to 2.59e-5
 
 print(f"The tree sequence now has {mts.num_mutations} mutations, "
       f"and mean pairwise nucleotide diversity is {mts.diversity()}.")
 
 # define sampling periods
-before = range(0, 10000, 50)
-during = range(9955, 10005, 5)
-after = range(10050, 10250, 50)
+before = range(0, 450, 50)
+during = range(450, 500, 5)
+after = range(500, 700, 50)
 
 sampling = [*before, *during, *after]    
 
-# initialize vectors
 pedigree_id = []
 het = []
 gen = [] 
 
-# loop through sampling periods... 
 for n in sampling: 
-	# for all individuals sampled at time point n, calculate individual heterozygosity
     ind_nodes = []
-    for i in mts.individuals_alive_at(n): # for each individual sampled... 
+    for i in mts.individuals_alive_at(n):
         ind = mts.individual(i)
         ind_nodes.append(ind.nodes)
     # the vector of per-individual heterozygosities:
-    ind_het = mts.diversity(ind_nodes, mode="site") # calculate heterozygosity
-    #mean_het = np.mean(ind_het)
+    ind_het = mts.diversity(ind_nodes, mode="site")
+   # mean_het = np.mean(ind_het)
 
-    # find pedigree IDs for sampled individuals
+    # save output
     x = []
     for i in mts.individuals_alive_at(n):
         ind = mts.individual(i)
-        label = f"slim_{ind.metadata['pedigree_id']}" 
+        label = f"slim_{ind.metadata['pedigree_id']}"
         x.append(label)
-        
-    # save pedigree ID, individual heterozygosity, and sampling point/generation
-    pedigree_id = np.append(pedigree_id, x) # append pedigree ID
-    het = np.append(het, ind_het) # append het 
-    gen = np.append(gen, np.repeat(n, len(ind_het))) # append sampling point
+    pedigree_id = np.append(pedigree_id, x) 
+    het = np.append(het, ind_het)
+    gen = np.append(gen, np.repeat(n, len(ind_het))) 
 
 
 het_data = { 'pedigree_id':pedigree_id, 
@@ -78,6 +76,5 @@ het_data = { 'pedigree_id':pedigree_id,
             'gen':gen,
            }
 
-# save data
 panda_df = pd.DataFrame(data=het_data)
 panda_df.to_csv(outdir+"/"+prefix+"_pi.txt", sep=' ', index=True)
