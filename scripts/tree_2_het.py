f10a559065802b1550f0b6ec063526149d3b8203
MeaghanClark <meaghaniclark@gmail.com>
2022-06-02 19:41:30 -0400

    adding code to calculate genetic relatedness from tree sequences

--- scripts/tree_2_het.py
+++ scripts/tree_2_het.py
@@ -7,7 +7,7 @@ import pandas as pd
 import random
 
 # uncomment these lines when running from command line
-# sys.argv = ['tree_processing.py', '../slim_output_05252022/tree_nWF_test_1.trees', '/Users/meaghan/Desktop/DC_slim/het', 'mu_test', 1e-8, 5]
+#sys.argv = ['tree_processing.py', '../slim_output_05312022/tree_nWF_100_1.trees', '/Users/meaghan/Desktop/DC_slim/het', 'relatedness_test', 1e-8, 5]
 # [0] -- python script name
 # [1] -- tree file
 # [2] -- outdir
@@ -35,7 +35,7 @@ orig_ts = pyslim.load(treefile)
 print(f"Loaded tree file")
 
 # recapitate tree
-rts = pyslim.recapitate(orig_ts, recombination_rate = 1e-8, ancestral_Ne=4232)
+rts = pyslim.recapitate(orig_ts, recombination_rate = 1e-8, ancestral_Ne=5487)
 
 orig_max_roots = max(t.num_roots for t in orig_ts.trees())
 recap_max_roots = max(t.num_roots for t in rts.trees())
@@ -60,35 +60,52 @@ after = range(500, 750, 50)
 sampling = [*before, *during, *after]
 print(sampling)
 
-# caculate heterozygosity
+# caculate heterozygosity and relatedness 
 pedigree_id = []
 het = []
 gen = [] 
+relatedness = []
+
 
 for n in sampling: 
     ind_nodes = []
     for i in mts.individuals_alive_at(n):
         ind = mts.individual(i)
         ind_nodes.append(ind.nodes)
-    # the vector of per-individual heterozygosities:
+        
+    # make vector of per-individual heterozygosities:
     ind_het = mts.diversity(ind_nodes, mode="site")
-   # mean_het = np.mean(ind_het)
-
-    # save output
+   
+    # make vector of pedigree_ids 
     x = []
     for i in mts.individuals_alive_at(n):
         ind = mts.individual(i)
         label = f"slim_{ind.metadata['pedigree_id']}"
         x.append(label)
+    
+    # make vector of genetic relatedness 
+    relate = []
+    for i in range(0, len(mts.individuals_alive_at(n))):
+        pairs = [(i, j) for j in range(len(ind_nodes))] # make pairs
+        relatedness_all = mts.genetic_relatedness(ind_nodes, indexes = pairs)
+        relatedness_noself = np.delete(relatedness_all, i)
+        relatedness_mean = np.mean(relatedness_noself)
+        relate.append(relatedness_mean)
+        
+    # save output for nth sampling point
     pedigree_id = np.append(pedigree_id, x) 
     het = np.append(het, ind_het)
-    gen = np.append(gen, np.repeat(n, len(ind_het))) 
+    gen = np.append(gen, np.repeat(n, len(ind_het))) # generation
+    relatedness = np.append(relatedness, relate)
 
-
+# assemble into dictionary
 het_data = { 'pedigree_id':pedigree_id, 
             'het':het, 
+            'relatedness':relatedness,
             'gen':gen,
            }
+
+# convert to dataframe and export
 
 panda_df = pd.DataFrame(data=het_data)
 panda_df.to_csv(outdir+"/"+prefix+"_het.txt", sep=' ', index=True)
