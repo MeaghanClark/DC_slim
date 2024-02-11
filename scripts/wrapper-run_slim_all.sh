9270935fc08be3277c45fc1fc8a7b0bab06ab30d
MeaghanClark <meaghaniclark@gmail.com>
2024-02-11 16:20:35 -0500

    changes to slim wrapper

--- scripts/wrapper-run_slim_all.sh
+++ scripts/wrapper-run_slim_all.sh
@@ -15,59 +15,15 @@ then
 elif [[ $sim_block == "high" ]]
 then
 	array_key=/mnt/home/clarkm89/DC_slim/scripts/array_index_key_10_20.txt
-elif [[ $sim_block == "WF" ]]
+elif [[ $sim_block == "pWF" ]]
 then
 	array_key=/mnt/home/clarkm89/DC_slim/scripts/array_index_key_1.txt
 else
 	echo simulation type invalid
 fi
-# slim specific variables
-#n=61525 # census op size
-#res=100 # reps of slimulation to run 
-#avg_age=20
-#r=100 # factor to reduce op size by
-
-# select burn-in based on avg_age (Ne will change with age, so burn in must also change!) burn-in = avg_age * 10 * Ne
-	# Ne estimated in estimate_Ne.R
-
-# 07292023: trying avg_Age * 2 * Ne instead
-# 01282024: will secify in array key
-#if [[ $avg_age == 2 ]]
-#then
-#	burn=126260
-#	burn=126300
-#	burn=25300
-#	burn=120000 # 01282024, for Ne of around 30000
-#elif [[ $avg_age == 5 ]]
-#then
-#	burn=315890
-#	burn=315900
-#	burn=63200
-#	burn=300000
-#elif [[ $avg_age == 10 ]] 
-#then
-#	burn=635576
-#	burn=635600
-#	burn=127150
-#	burn=600000
-#elif [[ $avg_age == 20 ]] 
-#then
-#	burn=1264558
-#	burn=1264600
-#	burn=253000
-#	burn=1200000
-
-#elif [[ $avg_age == 1 ]]
-#then
-#	burn=100000
-#else
-#	echo average age is invalid
-#fi
-
 
 #define uper-level variables:
 date=$(date +%m%d%Y)
-# header=${jobname}_${avg_age}_${r} # header name, can change
 run_name=DC_slim #label to use on outut files
 
 # define dirs
@@ -103,10 +59,14 @@ fi
 #check if logfiles directory has been created in submit dir yet; if not, make one
 if [ ! -d $logfilesdir ]; then mkdir $logfilesdir; fi
 
+
+job_no=$(wc -l < "$array_key")
+echo "The number of jobs we are requestion is $job_no"
+
 #submit job to cluster
 	# for re in $(seq 1 $reps) ; do 
 		sbatch --job-name=$jobname \
-		--array=1-600 \
+		--array=1-$job_no \
 		--export=JOBNAME=$jobname,DATE=$date,SLIMSCRIPT=$slimscript,ARRAY_KEY=$array_key,SIM_BLOCK=$sim_block,HEADER=$header,CPUS=$cpus,RUN_NAME=$run_name,STORAGENODE=$storagenode,OUTDIR=$outdir,INDIR=$indir,HOMEDIR=$homedir,LOGFILESDIR=$logfilesdir,EXECUTABLE=$executable \
 		--cpus-per-task=$cpus \
 		--mem-per-cpu=$ram_per_cpu \
